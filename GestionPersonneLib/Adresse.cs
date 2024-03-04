711135dd80a7c4558d81b043b416e2e18a1fb23c
SamuelMK-MK <samuelmbikamboli@gmail.com>
2024-03-04 12:47:25 +0200

    initial commit

---
+++ GestionPersonneLib/Adresse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GestionPersonneLib
+{
+    class Adresse:IAdresse
+    {
+        public Adresse() { 
+        }
+
+        private int Id;
+
+        public int Id1
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+        private string Nom;
+
+        public string Nom1
+        {
+            get { return Nom; }
+            set { Nom = value; }
+        }
+        private string Postnom;
+
+        public string Postnom1
+        {
+            get { return Postnom; }
+            set { Postnom = value; }
+        }
+        private string Adresse;
+
+        public string Adresse1
+        {
+            get { return Adresse; }
+            set { Adresse = value; }
+        }
+    }
+}
---
+++ GestionPersonneLib/IAdresse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GestionPersonneLib
+{
+    interface IAdresse
+    {
+        int Id { get; set; }       
+        string Nom { get; set; }
+        string Postnom { get; set; }
+        string Adresse { get; set; }
+    }
+}
